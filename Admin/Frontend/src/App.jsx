8c1620878f792e64d20913f008dee5b851d79ee1
bonnerjallah <121054718+bonnerjallah@users.noreply.github.com>
2023-12-29 23:56:55 -0500

    first commit

---
+++ Admin/Frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import {Route, RouterProvider, createBrowserRouter, createRoutesFromElements} from "react-router-dom"
+
+
+import Home from "./pages/Home"
+
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<Home /> } />
+    </Route>
+  )
+)
+
+
+function App() {
+
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  )
+}
+
+export default App
