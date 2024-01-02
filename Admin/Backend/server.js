f541620b978f2220733a325051f0a9cc55bc9f98
bonnerjallah <121054718+bonnerjallah@users.noreply.github.com>
2024-01-01 21:28:26 -0500

    created login api and middleware to validate access token

--- Admin/Backend/server.js
+++ Admin/Backend/server.js
@@ -1,6 +1,11 @@
 const express = require("express")
 const cors = require("cors")
 const mongoose = require("mongoose")
+const multer = require("multer")
+const path = require("path")
+
+const jwt = require("jsonwebtoken")
+const cookieParser = require("cookie-parser")
 
 
 const bcrypt = require("bcrypt")
@@ -15,14 +20,21 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-app.use(cors({
-    origin: ['http://localhost:5173'],
-    methods: ["GET", "POST", "PUT", "DELETE"]
-}))
-
-
 
 mongoose.connect("mongodb://localhost:27017/mylibrary")
+
+const jwtSec = process.env.VITE_jwtSecret
+const refshToken = process.env.VITE_jwtSecret
+
+
+app.use(cookieParser())
+
+
+app.use(cors({
+    origin: ['http://localhost:5174'],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}))
 
 
 
@@ -42,19 +54,87 @@ app.post("/registeradminusers", async (req, res) => {
 
     } catch (error) {
         console.error("Error adding admin user", error)
-        res.status(500).json({message: "Interna server issue"})
+        res.status(500).json({message: "Internal server issue"})
     }
 })
 
-app.get("/adminusers", async (req, res) => {
+app.post("/loginadminusers", async (req, res) => {
     try {
-        const result = await AdminUsers.find().exec();
-        res.json(result);
+        const {username, password} = req.body
+
+        if(!username || !password) {
+            return res.status(400).json({ message: "Both username and password are required" });
+        }
+
+        const results = await AdminUsers.findOne({username})
+
+        if(results) {
+            const hashedPassword = results.password
+
+            const passwordMatch = await bcrypt.compare(password, hashedPassword)
+
+            if(passwordMatch) {
+                const userData = {
+                    id: results._id,
+                    firstname: results.firstname,
+                    lastname: results.lastname,
+                    username: results.username
+                }
+
+                //Generate a JWT token
+                const accessToken = jwt.sign({user: userData}, jwtSec, {expiresIn: "10m"})
+                const refreshToken = jwt.sign({user: userData}, refshToken, {expiresIn: "1h"})
+
+                //Send token to client
+                res.cookie("token", accessToken)
+                res.cookie("refreshToken", refreshToken, { httpOnly: true })
+
+                return res.status(200).json({message: "Login Successful", userData:userData})
+
+            } else {
+                return res.status(401).json({message: "Invalid Password"})
+            }
+        } else {
+            return res.status(404).json({message: "User not found"})
+        }
+
     } catch (error) {
-        console.error("Error fetching users:", error);
-        res.status(500).json({ message: "An internal server issue" });
+        console.error("Error login in user", error)
+        res.status(500).json({message: "Internal server issue"})
     }
-});
+})
+
+
+//Middleware to validate access token
+const validateAccessToken = (req, res, next) => {
+    const accessToken = req.cookies.token; //Because the token was sent with cookie
+    if(!accessToken) {
+        return res.status(401).json({error: "Access token is missing"})
+    }
+
+    jwt.verify(accessToken, jwtSec, (err, decoded) => {
+        if(err) {
+            console.error("Error verifying access token", err)
+            return res.status(401).json({error: "Invalid access token"})
+        }
+
+        //Iftoken is valid, set the user information in req.user
+        req.user = decoded.user
+        next()
+    })
+}
+
+
+
+// app.get("/adminusers", async (req, res) => {
+//     try {
+//         const result = await AdminUsers.find().exec();
+//         res.json(result);
+//     } catch (error) {
+//         console.error("Error fetching users:", error);
+//         res.status(500).json({ message: "An internal server issue" });
+//     }
+// });
 
 
 
--- Admin/Frontend/src/components/LoginForm.jsx
+++ Admin/Frontend/src/components/LoginForm.jsx
@@ -6,24 +6,51 @@ import userloginstyle from "../styles/userloginstyle.module.css"
 
 const LoginForm = () => {
 
-  
+  const [loginData, setLoginData] = useState({
+    username: "",
+    password: ""
+  })
+
+  const handleLoginInput = (e) => {
+    const{name, value} = e.target
+
+    setUserLogin((prev) => ({...prev, [name]: value}))
+  }
+
+  const handleInputSubmit = async (e) => {
+    e.preventdefault()
+    try {
+      const response = await axios.post("http://localhost:3001/login", loginData, {
+        headers: {"Content-Type": "application/json"}
+      })
+
+      if(response.status === 200) {
+        console.log("successfully fetch userdata")
+      }
+    } catch (error) {
+      console.error("error fetching user", error)
+    }
+  }
+
+
+
 
   return (
     <>
         <div >
             <div className={userloginstyle.formWrapper}>
             <h2>Login</h2>
-            <form action="">
+            <form onSubmit={handleInputSubmit} >
                 <label htmlFor="user">Username: 
-                <input type="text" name='username' id='user' placeholder='Username' required />
+                <input type="text" name='username' id='user' value={loginData.username} placeholder='Username' onChange={handleLoginInput} required />
                 </label>
 
-                <label htmlFor="password">Password:
-                <input type="text" name='pwd' id='password' placeholder='Password' required />
+                <label htmlFor="pwd">Password:
+                <input type="text" name='password' id='pwd' value={loginData.password} placeholder='Password' onChange={handleLoginInput} required />
                 </label>
 
                 <div>
-                <button className={userloginstyle.loginButton}>Submit</button>
+                  <button className={userloginstyle.loginButton}>Submit</button>
                 </div>
             </form>
             </div>
