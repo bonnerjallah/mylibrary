f8055322c70efb4acde449de346196607d1e4c0e
bonnerjallah <121054718+bonnerjallah@users.noreply.github.com>
2024-02-25 23:49:06 -0500

    Added logout route

--- Webpage/Backend/server.js
+++ Webpage/Backend/server.js
@@ -169,6 +169,14 @@ app.get("/libraryusers", async(req, res) => {
     }
 })
 
+//Logout route
+app.post("/logout", (req, res) => {
+    res.clearCookie("token", {httpOnly: true, sameSite: "None", secure: true})
+    res.clearCookie("refreshtoken", {httpOnly: true, sameSite: "None", secure: true})
+
+    res.status(200).json({message: "Logged out successfully"})
+})
+
 app.listen(3001, () => {
     console.log("listening to port 3001")
 })
