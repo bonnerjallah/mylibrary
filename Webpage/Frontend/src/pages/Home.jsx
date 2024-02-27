7b6f16e3b83273d3d906a03c39b06e5ab8f5feb4
bonnerjallah <121054718+bonnerjallah@users.noreply.github.com>
2024-02-26 23:15:21 -0500

    added image to the hero section on the home page

--- Webpage/Frontend/src/pages/Home.jsx
+++ Webpage/Frontend/src/pages/Home.jsx
@@ -4,7 +4,9 @@ import homestyle from "../styles/homestyle.module.css"
 const Home = () => {
   return (
     <div>
-      <div className={homestyle.heroSection} ></div>
+      <div className={homestyle.heroSection} >
+        <img src="/home.jpg" alt="" />
+      </div>
     </div>
   )
 }
