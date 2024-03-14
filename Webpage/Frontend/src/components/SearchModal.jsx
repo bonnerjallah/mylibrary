d7877f59d0701a23857abcea7c25e549e359fabc
bonnerjallah <121054718+bonnerjallah@users.noreply.github.com>
2024-03-14 17:49:44 -0400

    Added search functionality to search modal

--- Webpage/Frontend/src/components/SearchModal.jsx
+++ Webpage/Frontend/src/components/SearchModal.jsx
@@ -1,4 +1,4 @@
-import { Search } from "lucide-react"
+import { Plus, Search } from "lucide-react"
 import shelfstyle from "../styles/shelfstyle.module.css"
 
 import { useState, useEffect } from "react"
@@ -6,9 +6,15 @@ import axios from "axios"
 
 const SearchModal = ({closeModal}) => {
 
+    const [searchCatagory, setSearchCatagory] = useState("")
+    const [searchInput, setSearchInput] = useState({
+        search: "" 
+    })
+    const [filteredBookData, setFilterBookData] = useState([])
+
     const [allBooks, setAllBooks] = useState([])
     const [currentPage, setCurrentPage] = useState(1)
-    const [booksPerPage, setBooksPerPage] = useState(15)
+    const [booksPerPage] = useState(10)
 
 
     useEffect(() => {
@@ -31,25 +37,48 @@ const SearchModal = ({closeModal}) => {
             fetchBooksData()
     }, [])
 
-    console.log("allBooks", allBooks)
-
     const lastBookIndex = currentPage * booksPerPage
     const firstBookIndex = lastBookIndex - booksPerPage
-    const currentBookPage = allBooks.slice(firstBookIndex, lastBookIndex)
+    const currentBookPage = filteredBookData.length > 0 ? filteredBookData.slice(firstBookIndex, lastBookIndex) :  allBooks.slice(firstBookIndex, lastBookIndex)
 
     //Pagination
     const pageNumber = []
 
-    const totalSuggestedBooks = allBooks.length
+    const totalBooks = filteredBookData.length > 0 ? filteredBookData.length : allBooks.length;
     const booksToShowPerPage = booksPerPage
 
-    for(let i = 1; i <= Math.ceil(totalSuggestedBooks / booksToShowPerPage); i++) {
+    for(let i = 1; i <= Math.ceil(totalBooks / booksToShowPerPage); i++) {
         pageNumber.push(i)
     }
 
     //Fuction to move pages
     const handleNextPage = (nextPage) => {
         setCurrentPage(nextPage)
+    }
+
+    const searchInputData = (e) => {
+        const {name, value} = e.target
+        setSearchInput((prev) => ({
+            ...prev,
+            [name] : value
+        }))
+    }
+
+    const handleSearchSubmit =  (e) => {
+        e.preventDefault()
+
+        const filteredBook = allBooks.filter(elem => {
+            if(searchCatagory === "title") {
+                return elem.bookTitle.toLowerCase().includes(searchInput.search.toLowerCase())
+            } else if (searchCatagory === "author") {
+                return elem.bookAuthor.toLowerCase().includes(searchInput.search.toLowerCase())
+            }
+
+            return false
+        })
+
+        setFilterBookData(filteredBook)
+        setSearchInput("")
     }
 
 
@@ -62,24 +91,47 @@ const SearchModal = ({closeModal}) => {
                     <p onClick={(e) => {closeModal(false)}}>X</p>
                 </div>
                 <div className={shelfstyle.searchContainer}>
-                <p style={{marginBottom:".5rem", fontSize:"1.2rem"}}>Search by Keyword</p>
-                    <form>
+                    <p style={{marginBottom:".5rem", fontSize:"1.2rem"}}>
+                        Search by Keyword: 
+                        <select onChange={(e) => {setSearchCatagory(e.target.value)}} style={{marginLeft:".5rem"}}>
+                            <option value=""></option>
+                            <option value="title">Title</option>
+                            <option value="author">Author</option>
+                        </select>
+                    </p>
+                    <form onSubmit={handleSearchSubmit}>
                         <label htmlFor="Search"></label>
-                        <input type="text" name="search" id="Search"  />
-                        <div className={shelfstyle.searchIconWrapper}>
-                            <Search size={48} />
+                        <input type="text" name="search" id="Search" value={searchInput.search} onChange={searchInputData} />
+                        <div  className={shelfstyle.searchIconWrapper}>
+                            <button type="submit"><Search size={48} /></button>
                         </div>
                     </form>
-                    <div>
+                    <div className={shelfstyle.pageNumberWrapper} style={{marginTop:".5rem"}}>
+                        {pageNumber.map((nextPage) => (
+                            <li key={nextPage} onClick={() => handleNextPage(nextPage)} style={{backgroundColor: currentPage === nextPage ? "white" : "black", color: currentPage === nextPage ? "black" : "white"}}>
+                                {nextPage}
+                            </li>
+                        ))}
+                    </div>
+                    <div className={shelfstyle.booksContainer}>
                         {currentBookPage && currentBookPage.map((elem, index) => (
-                            <div key={index} >
-                                {elem.bookTitle}
+                            <div key={index} className={shelfstyle.booksWrapperWrapper} >
+                                <div className={shelfstyle.bookWrapper}>
+                                    <img src={`http://localhost:3001/booksimages/${elem.bookImageUrl}`} alt="book image" width="60" height="65" />
+                                    <div>
+                                        {elem.bookTitle}
+                                    </div>
+                                </div>
+                                
+                                <div className={shelfstyle.addToWrapper}>
+                                    <p ><Plus size={28} className={shelfstyle.addBookPlus}/> Add</p>
+                                </div>
                             </div>
                         ))}
                     </div>
-                    <div>
+                    <div className={shelfstyle.pageNumberWrapper} style={{marginTop:".5rem"}}>
                         {pageNumber.map((nextPage) => (
-                            <li key={nextPage} onClick={() => handleNextPage(nextPage)}>
+                            <li key={nextPage} onClick={() => handleNextPage(nextPage)} style={{backgroundColor: currentPage === nextPage ? "white" : "black", color: currentPage === nextPage ? "black" : "white"}}>
                                 {nextPage}
                             </li>
                         ))}
